62c7bfea15e777257201b8dbe9afb096c972a548
masatora <atarashi.masatora@gmail.com>
2021-03-30 05:51:40 +0900

    Implement Fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,67 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// screen range are between 16384 to 24575(16384+(32*256-1))
+// 32*256-1=8191
+
+@LOOP
+0;JMP
+@reseted
+M=0
+
+(LOOP)
+  @reseted
+  D=M
+  @RESET
+  D;JEQ
+  @KBD
+  D=M
+  @BLACK
+  D;JNE
+  @WHITE
+  D;JEQ
+  @LOOP
+  0;JMP
+
+(BLACK)
+  @reseted
+  M=0
+  @i
+  A=M
+  M=-1
+  @i
+  M=M+1
+  D=M
+  @24575
+  D=D-A
+  @LOOP
+  D;JGT
+  @BLACK
+  0;JMP
+
+(RESET)
+  @SCREEN
+  D=A
+  @i
+  M=D
+  @reseted
+  M=1
+  @LOOP
+  0;JMP
+ 
+(WHITE)
+  @reseted
+  M=0
+  @i
+  A=M
+  M=0
+  @i
+  M=M+1
+  D=M
+  @24575
+  D=D-A
+  @LOOP
+  D;JGT
+  @WHITE
+  0;JMP
