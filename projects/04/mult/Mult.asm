bd18de5c42191b81f0369e9bb53dfbeda69d0a4a
masatora <atarashi.masatora@gmail.com>
2021-03-30 00:48:51 +0900

    Implement Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,21 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+  @R2
+  M=0   //R2=0
+(LOOP)
+  @R1
+  D=M
+  @END
+  D;JLE   //if(R1<=0) then end
+  @R0   //D=R0
+  D=M
+  @R2
+  M=D+M   //R2=R2+R0
+  @R1
+  M=M-1   //R1=R1-1
+  @LOOP
+  0;JMP
+(END)
+  @END
+  0;JMP
